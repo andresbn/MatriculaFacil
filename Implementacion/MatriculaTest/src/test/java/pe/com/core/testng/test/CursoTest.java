a7ec7a1206cda420df35e1697d34b0bd29b24621
Roy Taza <roytazarojas@gmail.com>
2014-11-13 17:36:40 -0500

    Finalizado pruebas unitarias de Cursos. Agregadas pruebas de correcto e incorrecto con sus respectivas prioridades.

--- Implementacion/MatriculaTest/src/test/java/pe/com/core/testng/test/CursoTest.java
+++ Implementacion/MatriculaTest/src/test/java/pe/com/core/testng/test/CursoTest.java
@@ -8,7 +8,6 @@ package pe.com.core.testng.test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.testng.Assert;
 import java.util.List;
-import java.util.Random;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -22,7 +21,7 @@ import pe.com.core.model.Curso;
 public class CursoTest {
 
     ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-    private CursoDAO cursoDAO = context.getBean(CursoDAO.class);
+    private final CursoDAO cursoDAO = context.getBean(CursoDAO.class);
 
     @BeforeClass
     public static void inicioClase() {
@@ -34,19 +33,8 @@ public class CursoTest {
         System.out.println("Final de la Clase");
     }
 
-    @Test(priority = 1)
-    public void listCurso() {
-        try {
-            List<Curso> listCursos = cursoDAO.list();
-            Assert.assertNotEquals(listCursos.size(), 0);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Assert.fail("Fallo: " + e.getMessage());
-        }
-    }
-
     @Test(priority = 0)
-    public void saveCurso() {
+    public void saveCursoCorrecto() {
         try {
             Curso curso = new Curso();
             curso.setCiclo(6);
@@ -57,19 +45,91 @@ public class CursoTest {
             cursoDAO.save(curso);
             Assert.assertNotEquals(curso.getIdCurso(), 0);
         } catch (Exception e) {
-            e.printStackTrace();
+            Assert.fail("Fallo: " + e.getMessage());
+        }
+    }
+
+    @Test(priority = 1)
+    public void listCurso() {
+        try {
+            List<Curso> listCursos = cursoDAO.list();
+            Assert.assertNotEquals(listCursos.size(), 0);
+        } catch (Exception e) {
             Assert.fail("Fallo: " + e.getMessage());
         }
     }
 
     @Test(priority = 2)
-    public void deleteCurso() {
+    public void updateCursoCorrecto() {
+        try {
+            List<Curso> listCursos = cursoDAO.list();
+            Curso curso = listCursos.get(listCursos.size() - 1);
+            curso.setCiclo(5);
+            curso.setCodigo("AS123");
+            curso.setCreditos(5);
+            curso.setNombre("Evolucion de Software - Modificado");
+            curso.setRequisitos("IHC - Modificado");
+            Assert.assertTrue(cursoDAO.update(curso));
+        } catch (Exception e) {
+            Assert.fail("Fallo: " + e.getMessage());
+        }
+    }
+
+    @Test(priority = 3)
+    public void deleteCursoCorrecto() {
         try {
             List<Curso> listCursos = cursoDAO.list();
             Assert.assertTrue(cursoDAO.delete(listCursos.get(listCursos.size() - 1)));
         } catch (Exception e) {
-            e.printStackTrace();
             Assert.fail("Fallo: " + e.getMessage());
         }
     }
+
+    @Test(priority = 5)
+    public void saveCursoIncorrecto() {
+        try {
+            String codigoCurso = "";
+            Curso curso = new Curso();
+            curso.setCiclo(5);
+            for (int i = 0; i < 60; i++) {
+                codigoCurso += "a";
+            }
+            curso.setCodigo(codigoCurso);
+            curso.setCreditos(5);
+            curso.setNombre("Evolucion de Software - Modificado");
+            curso.setRequisitos("IHC - Modificado");
+            cursoDAO.save(curso);
+            Assert.assertEquals(curso.getIdCurso(), 0);
+        } catch (Exception e) {
+            Assert.fail("Fallo: " + e.getMessage());
+        }
+    }
+
+    @Test(priority = 6)
+    public void updateCursoIncorrecto() {
+        try {
+            Curso curso = new Curso();
+            curso.setIdCurso(-1);
+            curso.setCiclo(5);
+            curso.setCodigo("AS123");
+            curso.setCreditos(5);
+            curso.setNombre("Evolucion de Software - Modificado");
+            curso.setRequisitos("IHC - Modificado");
+            Assert.assertFalse(cursoDAO.update(curso));
+        } catch (Exception e) {
+            Assert.fail("Fallo: " + e.getMessage());
+        }
+    }
+
+    @Test(priority = 7)
+    public void deleteCursoIncorrecto() {
+        try {
+            Curso curso = new Curso();
+            curso.setIdCurso(-1);
+            Assert.assertFalse(cursoDAO.delete(curso));
+        } catch (Exception e) {
+            Assert.fail("Fallo: " + e.getMessage());
+        }
+    }
+
 }
